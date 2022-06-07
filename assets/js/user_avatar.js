fccdbb120f5635f26efae96e6b600a084390df97
zz <zhaosdjzu@gmail.com>
2022-06-07 10:19:18 +0800

    提交了个人中心修改

--- assets/js/user_avatar.js
+++ assets/js/user_avatar.js
@@ -17,4 +17,26 @@ $(function () {
     var imgURL = URL.createObjectURL(fileInfo);
     $image.cropper("destroy").attr("src", imgURL).cropper(options);
   });
+
+  $("#btnUpload").on("click", function () {
+    var dataURL = $image
+      .cropper("getCroppedCanvas", {
+        width: 160,
+        height: 90,
+      })
+      .toDataURL();
+    $.ajax({
+      method: "POST",
+      url: "/my/update/avatar",
+      data: {
+        avatar: dataURL,
+      },
+      success: function (res) {~
+        if (res.status !== 0)
+          return layui.layer.msg("更换头像失败", { icon: 5 });
+        layui.layer.msg("更换头像成功", { icon: 6 });
+        window.parent.getUserInfo();
+      },
+    });
+  });
 });
